a3af01a6b12cd34b46dc766c955ffb0ac42a9f39
PhoeniXAbhisheK <abhiy3010@gmail.com>
2018-04-09 20:13:47 +0530

    Changes made for better params & unwanted code removed

--- TwitterBot/bot.js
+++ TwitterBot/bot.js
@@ -1,77 +1,47 @@
-const twit = require('twit');
-const config = require('./config.js');
+const
+  twit = require('twit'),
+  config = require('./config');
 
 const Twitter = new twit(config);
 
+const paramText = ['Include', 'multiple', 'parameters', 'seperated', 'by', 'comma'];
+
+// function to generate a random tweet 
+function ranDom(arr) {
+  const index = Math.floor(Math.random() * arr.length);
+  return arr[index];
+};
+// RETWEET BOT ==========================
+
+// find latest tweet according the query 'q' in params
 const retweet = function() {
+  const time = new Date(Date.now());
   const params = {
-    q: '#js, #javascript',
+    q: ranDom(paramText),
     result_type: 'recent',
     lang: 'en'
   }
-
   Twitter.get('search/tweets', params, function(err, data) {
-    // if there no errors
+
     if (!err) {
-      // grab ID of tweet to retweet
-      var retweetId = data.statuses[0].id_str;
+
+      const tweet = data.statuses;
+      const randomTweet = ranDom(tweet);
       // Tell TWITTER to retweet
-      Twitter.post('statuses/retweet/:id', {
-        id: retweetId
-      }, function(err, response) {
+      Twitter.post('statuses/retweet/:id', { id: randomTweet.id_str }, function(err, response) {
         if (response) {
-          console.log('Retweeted!!!');
-        }
-        // if there was an error while tweeting
-        if (err) {
-          console.log('Something went wrong while RETWEETING... Duplication maybe...');
+          console.log('Retweet success');
+        } else {
+          console.log('Retweet failure');
         }
       });
-    }
-    // if unable to Search a tweet
-    else {
+    } else {
       console.log('Something went wrong while SEARCHING...');
     }
   });
 }
 
+// grab & retweet as soon as program is running...
 retweet();
 
-setInterval(retweet, 300000)
-
-// function to generate a random tweet tweet
-function generateRandom (arr) {
-  var index = Math.floor(Math.random() * arr.length);
-  return arr[index];
-};
-
-const favourite = function() {
-  const params = {
-    q: '#js, #javascript',
-    result_type: 'recent',
-    lang: 'en'
-  }
-
-  Twitter.get('search/tweets', params, function(err, data) {
-
-    // find tweets
-    var tweet = data.statuses;
-    var randomTweet = generateRandom(tweet); // pick a random tweet
-    // if random tweet exists
-    if (typeof randomTweet != 'undefined') {
-      // Tell TWITTER to 'favorite'
-      Twitter.post('favorites/create', { id: randomTweet.id_str }, function(err, response) {
-        // if there was an error while 'favorite'
-        if (err) {
-          console.log('CANNOT BE FAVORITE... Error');
-        } else {
-          console.log('FAVORITED... Success!!!');
-        }
-      });
-    }
-  });
-}
-
-favourite();
-
-setInterval(favourite, 300000);
+setInterval(retweet, 30000);
